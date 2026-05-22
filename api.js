27b321d53fe9b31a87536b10ef118e4678f5fc28
unknown <rufinoguilherme633@gmail.com>
2026-05-22 08:04:01 -0300

    BASE URL BACKEND JAVA

--- api.js
+++ api.js
@@ -1,1 +1,2 @@
-export const BASE_URL_JAVA_BACKEND = import.meta.env.BASE_URL_JAVA_BACKEND;
+export const BASE_URL_JAVA_BACKEND =
+  import.meta.env.BASE_URL_JAVA_BACKEND || "http://localhost:8080";
